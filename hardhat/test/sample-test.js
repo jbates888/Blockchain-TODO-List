3bec5ad8af456ab769b00fa808d4e68ceb823131
jack bates <jackbates555@gmail.com>
2022-04-10 21:36:02 -0500

    update

--- hardhat/test/sample-test.js
+++ hardhat/test/sample-test.js
@@ -11,7 +11,7 @@ describe("Greeter", function () {
 
     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
-    // wait until the transaction is mined
+    // wait until the transaction is mined test commit
     await setGreetingTx.wait();
 
     expect(await greeter.greet()).to.equal("Hola, mundo!");
